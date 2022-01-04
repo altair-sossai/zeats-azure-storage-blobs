3ee3e3be41e78c9875a3036696892b73beb72947
Altair Sossai <altair_sossai@outlook.com>
2022-01-04 09:48:31 -0800

    upgrade to net6.0

--- Zeats.Azure.Storage.Blobs.UnitTest/UnitOfWork/UnitOfWorkAzureStorageBlobsTest.cs
+++ Zeats.Azure.Storage.Blobs.UnitTest/UnitOfWork/UnitOfWorkAzureStorageBlobsTest.cs
@@ -3,41 +3,40 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Zeats.Azure.Storage.Blobs.UnitOfWork;
 using Zeats.Azure.Storage.Blobs.UnitTest.Factories;
 
-namespace Zeats.Azure.Storage.Blobs.UnitTest.UnitOfWork
+namespace Zeats.Azure.Storage.Blobs.UnitTest.UnitOfWork;
+
+[TestClass]
+public class UnitOfWorkAzureStorageBlobsTest
 {
-    [TestClass]
-    public class UnitOfWorkAzureStorageBlobsTest
+    private UnitOfWorkAzureStorageBlobs _unitOfWork;
+
+    [TestInitialize]
+    public void Initialize()
     {
-        private UnitOfWorkAzureStorageBlobs _unitOfWork;
+        _unitOfWork = UnitOfWorkAzureStorageBlobsFactory.New();
+    }
 
-        [TestInitialize]
-        public void Initialize()
-        {
-            _unitOfWork = UnitOfWorkAzureStorageBlobsFactory.New();
-        }
+    [TestMethod]
+    public async Task CreateBlobContainerAsync()
+    {
+        var blobContainerClient = await _unitOfWork.CreateBlobContainerIfNotExistsAsync("unit-test");
 
-        [TestMethod]
-        public async Task CreateBlobContainerAsync()
-        {
-            var blobContainerClient = await _unitOfWork.CreateBlobContainerIfNotExistsAsync("unit-test");
+        Assert.IsNotNull(blobContainerClient);
+    }
 
-            Assert.IsNotNull(blobContainerClient);
-        }
+    [TestMethod]
+    public async Task GetBlobContainerClient()
+    {
+        var blobContainerClient = _unitOfWork.GetBlobContainerClient("unit-test");
 
-        [TestMethod]
-        public async Task GetBlobContainerClient()
-        {
-            var blobContainerClient = _unitOfWork.GetBlobContainerClient("unit-test");
+        Assert.IsNotNull(blobContainerClient);
+        Assert.IsTrue(await blobContainerClient.ExistsAsync());
+    }
 
-            Assert.IsNotNull(blobContainerClient);
-            Assert.IsTrue(await blobContainerClient.ExistsAsync());
-        }
-
-        [TestMethod]
-        public async Task GetBlobsAsync()
-        {
-            await foreach (var blobItem in _unitOfWork.GetBlobsAsync("unit-test"))
-                Assert.IsNotNull(blobItem);
-        }
+    [TestMethod]
+    public async Task GetBlobsAsync()
+    {
+        await foreach (var blobItem in _unitOfWork.GetBlobsAsync("unit-test"))
+            Assert.IsNotNull(blobItem);
     }
 }
--- Zeats.Azure.Storage.Blobs/Extensions/BlobServiceClientExtensions.cs
+++ Zeats.Azure.Storage.Blobs/Extensions/BlobServiceClientExtensions.cs
@@ -3,25 +3,24 @@ using Azure;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 
-namespace Zeats.Azure.Storage.Blobs.Extensions
+namespace Zeats.Azure.Storage.Blobs.Extensions;
+
+public static class BlobServiceClientExtensions
 {
-    public static class BlobServiceClientExtensions
+    public static async Task<BlobContainerClient> CreateBlobContainerIfNotExistsAsync(this BlobServiceClient blobServiceClient, string blobContainerName, PublicAccessType publicAccessType = PublicAccessType.None)
     {
-        public static async Task<BlobContainerClient> CreateBlobContainerIfNotExistsAsync(this BlobServiceClient blobServiceClient, string blobContainerName, PublicAccessType publicAccessType = PublicAccessType.None)
-        {
-            var blobContainerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
+        var blobContainerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
 
-            if (await blobContainerClient.ExistsAsync())
-                return blobContainerClient;
+        if (await blobContainerClient.ExistsAsync())
+            return blobContainerClient;
 
-            return await blobServiceClient.CreateBlobContainerAsync(blobContainerName, publicAccessType);
-        }
+        return await blobServiceClient.CreateBlobContainerAsync(blobContainerName, publicAccessType);
+    }
 
-        public static AsyncPageable<BlobItem> GetBlobsAsync(this BlobServiceClient blobServiceClient, string blobContainerName)
-        {
-            var blobContainerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
+    public static AsyncPageable<BlobItem> GetBlobsAsync(this BlobServiceClient blobServiceClient, string blobContainerName)
+    {
+        var blobContainerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
 
-            return blobContainerClient.GetBlobsAsync();
-        }
+        return blobContainerClient.GetBlobsAsync();
     }
 }
